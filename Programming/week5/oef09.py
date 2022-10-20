986a7f58a8ed7eebfcc1b2739a750a55b1136151
tim.bleuze <tim.bleuze@student.howest.be>
2022-10-20 12:16:51 +0200

    week5 afwerken

--- Programming/week5/oef09.py
+++ Programming/week5/oef09.py
@@ -8,22 +8,26 @@ import random
 
 def registreer_snelheidsmeting(lid):
     dic={}
-    dic[lid] = random.randint(30,70)
+    for i in lid:
+        dic[i] = random.randint(30,70)
     return dic
 
 def print_snelheden_personen(straatnaam, dic):
     print(f'{straatnaam}')
     for i in dic.keys():
-        print(f'{i}\t{dic[i]}')
+        print(f'{i:30s}{dic[i]}')
 
 def print_boetes(dic,snelheid):
+    print('Boete:')
     for i in dic.keys():
         if dic[i]>snelheid:
-            print(f'{i}\t boete: {(dic[i]-snelheid)*10}')
+            print(f'{i :30s} boete: {(dic[i]-snelheid)*10}')
 
 
 snelheid = int(input('geef de maximum toegelaten snelheid op: '))
 
 dic = registreer_snelheidsmeting(list_personeel)
-print(dic)
+print_snelheden_personen('Test', dic)
+print_boetes(dic,snelheid)
+
 
