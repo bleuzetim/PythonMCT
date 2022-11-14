a0e6b22e24a6fcc6f875e24beee3811272fbb332
tim.bleuze <tim.bleuze@student.howest.be>
2022-11-14 11:12:52 +0100

    Map name change

---
+++ Programming/Q1/opdracht3.py
@@ -0,0 +1,13 @@
+# Vul onderstaande functies aan. Vergeet de parameters niet aan te vullen (zie opgave)!!!
+# Test onderaan alle functies voldoende uit
+
+def geef_aantallen_per_provincie():
+    pass
+
+
+misses_2019 = {"Marieke": 8800, "Delfien": 8500, "Mieke": 8531, "Els": 9070, "Lola":  2500,
+               "Dolly": 9999, "Marianne": 9000, "Claudine": 2800, "Lies": 3080, "Jacqueline": 3720, "Jozefien": 8700}
+misses_2018 = {"Tine": 3700, "Anke": 8700, "Claudia": 8530, "Marijn": 9000,
+               "Sofie":  2650, "Marie": 9870, "Leen": 9000, "Conny": 2800}
+
+# Test hier bovenstaande functies uit (zie opgave voor meer detail)
